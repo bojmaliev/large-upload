d18b7c4ac44fd38d3569a0d5157b73263a2a62ce
Martin Bojmaliev <mbojmaliev@gmail.com>
2025-12-02 23:37:43 +0100

    ok

--- app/Filament/Resources/Uploads/Tables/UploadsTable.php
+++ app/Filament/Resources/Uploads/Tables/UploadsTable.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Uploads\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class UploadsTable
@@ -13,7 +14,8 @@ class UploadsTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('id')->sortable(),
+                TextColumn::make('source'),
             ])
             ->filters([
                 //
